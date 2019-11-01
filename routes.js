d11457f122e6880768e342f960c9d13734320995
luviv <lavo1999@hotmail.com>
2019-10-31 19:19:22 -0500

    routes

---
+++ routes.js
@@ -0,0 +1,41 @@
+const assert = require('assert');
+
+function createRoutes (app, db) {
+    
+    // todas las funciones que interactuen con la base de datos van aquí
+    app.get('/', (request, response) => {
+        response.sendFile(__dirname + '/public/home.html');
+
+        // seleccionamos la colección que necesitamos
+        const products = db.collection('products');
+
+        // buscamos todos los productos
+        products.find({})
+            // transformamos el cursor a un arreglo
+            .toArray((err, result) => {
+                // asegurarnos de que no hay error
+                assert.equal(null, err);
+
+                //
+                console.log(result);
+            });
+
+    });
+
+    app.get('/api/products', (request, response) => {
+        response.send(products);
+    })
+
+    app.post('/api/products', (request, response) => {
+        console.log(request.body);
+
+        const products = db.collection('products');
+        products.insert(request.body);
+
+        response.send({
+            message: 'ok',
+        });
+    });
+}
+
+module.exports = createRoutes;
