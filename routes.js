4ec2132f55a5c537fe8a041b883873c73c28dce8
luviv <lavo1999@hotmail.com>
2019-11-05 16:48:25 -0500

    CSS initiated

--- routes.js
+++ routes.js
@@ -46,6 +46,24 @@ function createRoutes (app, db) {
        
    })
 
+   app.get('/products/:id', (request, response) => {
+       var id = request.params.id;
+       console.log(id);
+
+       var products = db.collection('products');
+       products.find({"_id": new ObjectID(id)})
+        .toArray((err, result) => {
+            assert.equal(null, err);
+                
+                var context = {
+                    products: result[0]
+                };
+
+                console.log(context);
+                response.render('product', context);
+        });
+   });
+
     app.post('/api/products', (request, response) => {
         console.log(request.body);
 
