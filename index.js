eba66f360cbdd499e94c52f72e1dfe31f6927bac
luviv <lavo1999@hotmail.com>
2019-11-29 10:50:11 -0500

    herokuapp

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 
-const url = 'mongodb://localhost:27017';
+const url = 'mongodb+srv://finalweb:finalweb123@cluster0-xhwiv.mongodb.net/test?retryWrites=true&w=majority';
 const dbName = 'store';
 const client = new MongoClient(url);
 
@@ -35,7 +35,7 @@ client.connect(function(err) {
   
     createRoutes(app, db);
 
-    app.listen(5000, () => {
+    app.listen(process.env.PORT || 5000, () => {
         console.log('listening');
     });
 
